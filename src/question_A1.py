d9b7d0d2c35f18e0213fcdfd40e436f33cb8d594
dinildamsith <dinildamsith52@gmail.com>
2024-10-17 11:26:22 +0530

    changes added and jupyter note book added

--- src/question_A1.py
+++ src/question_A1.py
@@ -12,19 +12,25 @@ print(mean_profit_per_region)
 regions_to_discontinue = mean_profit_per_region[mean_profit_per_region['Profit'] < 27]['Region'].tolist()
 print(regions_to_discontinue)
 
+
+
 top_region = data[~data['Region'].isin(regions_to_discontinue)]
-print(top_region)
+print(top_region.head())
 
 plt.figure(figsize=(10, 6))
-plt.bar(mean_profit_per_region['Region'], mean_profit_per_region['Profit'], color='skyblue')
+plt.bar(mean_profit_per_region['Region'], mean_profit_per_region['Profit'], color='blue')
 plt.xlabel('Region')
 plt.ylabel('Mean Profit')
 plt.title('Mean Profit per Region')
 plt.axhline(y=27, color='red', linestyle='--', label='Discontinue Threshold (27)')
 plt.legend()
+
+
+ax = plt.gca()
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
 plt.show()
 
-
-print("Regions to be discontinued:", regions_to_discontinue)
-print("Top Regions DataFrame shape:", top_region.shape)
+print(regions_to_discontinue)
+print(top_region.shape)
 print(top_region.head())
